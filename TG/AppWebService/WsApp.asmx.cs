b6633c8d78f10a59a85ec37ad54394c580e6c37e
Victor Tavares Queiroz <tavaresqueiroz@gmail.com>
2012-10-15 23:22:46 -0300

    App removed code.

---
+++ TG/AppWebService/WsApp.asmx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace AppWebService
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://appwebservice.fatec.com.br/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WsApp : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string getPlans()
+        {
+            return "Get Plans";
+        }
+
+        [WebMethod]
+        public string getUser(String id)
+        {
+            return "Get User";
+        }
+    }
+}
---
+++ TG/TG.System/Controllers/WSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TG.Application.Controllers
+{
+    public class WSController : Controller
+    {
+        public ActionResult Login(string username, string password)
+        {
+            var obj = new { var = "teste" };
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
