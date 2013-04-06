7ca82b80f4469b4e0737d059152d3985b3a5aded
Victor Tavares Queiroz <tavaresqueiroz@gmail.com>
2013-04-05 22:03:52 -0300

    Tela de comunicação (fake)

---
+++ TG/TG.System/Controllers/ComunicacaoController.cs
@@ -0,0 +1,21 @@
+﻿using Plan5W2HPlusPlus.Application.ActionFilter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plan5W2HPlusPlus.Application.Controllers
+{
+    [NHibernateActionFilter(Order = 1)]
+    [AuthorizationActionFilter(Order = 2)]
+    [Authorize]
+    public class ComunicacaoController : LoggedController
+    {   
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
